38fad8e93c42e906c1f1de295cff1149c85c3af9
wuxiaodi <wuxiaodi@qq.com>
2022-04-21 21:22:05 +0800

    hotfix 2

--- src/main/java/com/Git.java
+++ src/main/java/com/Git.java
@@ -4,5 +4,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("1111");
         System.out.println("hotfix1");
+        System.out.println("hotfix2");
+
     }
 }
